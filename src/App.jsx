732c49f772aa50d77bd1e4df94ce1898e7fe6821
OG0808 <ogarns@proton.me>
2023-12-31 19:59:02 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Weather from './pages/Weather'
-
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Weather from "./pages/Weather";
 
 function App() {
-  
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Weather/>}/>
-        </Routes>
-
+        <Route path="/" element={<Weather />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
