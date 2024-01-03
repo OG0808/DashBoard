cc340e8f849787285998bac8d93cd7ed303fdb6f
OG0808 <ogarns@proton.me>
2024-01-02 22:05:49 -0500

    maps and routes

--- src/pages/maps/Maps.jsx
+++ src/pages/maps/Maps.jsx
@@ -1,4 +1,6 @@
-import "leaflet/dist/leaflet.css";
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Reutiliza las imágenes del paquete ~leaflet
+import 'leaflet-defaulticon-compatibility';
 import "./maps.css";
 import {
   MapContainer,
@@ -11,7 +13,6 @@ import { useState } from "react";
 import Layers from "../../components/LayersMaps/Layers";
 import useLayers from "../../store/useLayers";
 import useDarkmode from "../../store/useDarkmode";
-import Toggle from "../../components/Toggle/Toggle";
 
 const Maps = () => {
   const { darkMode } = useDarkmode();
@@ -46,11 +47,11 @@ const Maps = () => {
       <Layers />
       <MapContainer center={positionn} zoom={2} scrollWheelZoom={true}>
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <TileLayer
-          attribution='&copy; <a href="https://openweathermap.org/">OpenWeatherMap</a>'
+          attribution='© <a href="https://openweathermap.org/">OpenWeatherMap</a>'
           url={`https://tile.openweathermap.org/map/${
             layer || "temp_new"
           }/{z}/{x}/{y}.png?appid=61f7f94e8821c4e346c3d9ca5e7cde9e`}
